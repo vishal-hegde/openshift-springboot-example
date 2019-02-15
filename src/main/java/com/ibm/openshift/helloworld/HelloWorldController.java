920753858b4b54a22ab12b6d98c81d79f678ef9e
Vishal Hegde <vishheg1@in.ibm.com>
2019-02-15 06:28:34 +0530

    openshift hello world example

---
+++ src/main/java/com/ibm/openshift/helloworld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.ibm.openshift.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World from openshift";
+    }
+}
